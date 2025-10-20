c1c989dccff4b65d3d21ad3ff74f18c08269595b
RevenkoVladislav <Vlad_reven@mail.ru>
2025-10-20 21:44:12 +0600

    первые роуты и пустые контроллеры

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\NewsController;
 
-Route::get('/', function () {
-    return view('welcome');
+//Главная страница
+Route::controller(MainController::class)->group(function () {
+    Route::get('/', 'index');
+    Route::get('/about', 'about');
+    Route::get('/services', 'services');
+});
+
+Route::controller(NewsController::class)->group(function () {
+   Route::get('/news', 'news');
+   Route::get('/news/{id}', 'post');
 });

896733601984e4fafa76d17aebb1513de6ddde1f
RevenkoVladislav <Vlad_reven@mail.ru>
2025-10-21 15:32:14 +0600

    практика css, обертка float

---
+++ resources/views/index.blade.php
@@ -0,0 +1,126 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible">
+    <title>Index</title>
+    @vite(['resources/css/app.css', 'resources/css/style.css', 'resources/js/app.js'])
+</head>
+<body>
+<section>
+    <header>
+
+    </header>
+</section>
+
+<section>
+    <div class="box">
+        box1
+    </div>
+</section>
+
+<section>
+    <div class="wrap">
+        <div class="box-float">
+            Lorem ipsum dolor sit amet
+        </div>
+
+        <div class="box-float">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae debitis facere facilis magnam
+        </div>
+
+        <div class="box-float">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, quaerat unde. Ab alias consectetur
+            cum deleniti, dolore earum est fugiat in molestias, neque perferendis perspiciatis quaerat quas rerum soluta
+            voluptate.
+        </div>
+
+        <div class="box-float">
+            Lorem ipsum dolor sit amet
+        </div>
+
+        <div class="box-float">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae debitis facere facilis magnam
+        </div>
+
+        <div class="box-float">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, quaerat unde. Ab alias consectetur
+            cum deleniti, dolore earum est fugiat in molestias, neque perferendis perspiciatis quaerat quas rerum soluta
+            voluptate.
+        </div>
+
+        <div class="box-clear"></div>
+    </div>
+</section>
+
+<sector>
+    <div>
+        <ol>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ol>
+        <dl>
+            <dt>Тема</dt>
+            <dd>Пояснение</dd>
+        </dl>
+    </div>
+</sector>
+
+<section>
+    <div>
+        <p>Параграф <b>жирным</b> <i>курсив</i></p>
+    </div>
+</section>
+
+<section>
+    <table border="1">
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>Salary</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Влад</td>
+            <td>Ревенко</td>
+            <td>300</td>
+        </tr>
+        <tr>
+            <td>Марина</td>
+            <td>Ревенко</td>
+            <td>500</td>
+        </tr>
+        <tr>
+            <td>Соня</td>
+            <td>Свиркова</td>
+            <td>100</td>
+        </tr>
+        </tbody>
+    </table>
+</section>
+
+<section>
+    <aside>
+
+    </aside>
+</section>
+
+<section>
+    <article>
+
+    </article>
+</section>
+
+<section>
+    <footer>
+
+    </footer>
+</section>
+</body>
+</html>

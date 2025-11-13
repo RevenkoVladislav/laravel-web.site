1e469ccc641cf9b79188560000016aee4b5ea36f
RevenkoVladislav <Vlad_reven@mail.ru>
2025-11-13 23:50:46 +0600

    миграция для промежуточной таблицы

--- database/migrations/2025_11_13_173104_create_tags_table.php
+++ database/migrations/2025_11_13_173104_create_tags_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tags', function (Blueprint $table) {
-            $table->id();
+            $table->id()->primary()->autoIncrement()->unsigned();
             $table->string('name');
             $table->timestamps();
         });

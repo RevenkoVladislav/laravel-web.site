9680e16ac9167ac0b7153ca0ae2ce92b90f93320
RevenkoVladislav <Vlad_reven@mail.ru>
2025-11-16 23:40:44 +0600

    вывод курсов в json с учетом коэфициентов цен на странице /interesting

--- app/Http/Controllers/PriceController.php
+++ app/Http/Controllers/PriceController.php
@@ -4,25 +4,50 @@ namespace App\Http\Controllers;
 
 use App\Models\Course;
 use App\Models\School;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PriceController extends Controller
 {
     public function PriceForCourse(Course $course)
     {
         $schools = $course->schools()->withPivot('price')->orderByPivot('price', 'ASC')->get();
-        //return $schools;
+        //return compact('course', 'schools');
         return view('school.price-from-school', compact('course', 'schools'));
     }
 
-    public function GroupPrice(Course $course)
+    public function GroupPrice(Request $request)
     {
-
+        //докрутить валидацию
+        $courses = Course::with([
+            'schools' => fn(BelongsToMany $query) => $query->orderByPivot('price')
+        ])
+            ->whereIn('id', $request->query('id'))
+            ->get();
+        return $courses;
     }
 
     public function InterestingPrice(Course $course)
     {
+        //ищем курсы у котороых коэффициент цен меньше 0.9, получаем у них id курсов
+        $res = DB::table('schools')
+            ->select(['course_id', DB::raw('MIN(price) / AVG(price) as coef')])
+            ->join('course_school', 'schools.id', '=', 'course_school.school_id')
+            ->groupBy('course_id')
+            ->having('coef', '<', '0.9')
+            ->orderBy('coef')
+            ->limit(3)
+            ->pluck('course_id');
 
+
+        $courses = Course::with([
+            'schools' => fn(BelongsToMany $query) => $query->orderByPivot('price')->limit(2)
+        ])
+            ->whereIn('id', $res)
+            ->get();
+
+        return $courses;
     }
 
     public function SchoolCourse(School $school, Course $course)

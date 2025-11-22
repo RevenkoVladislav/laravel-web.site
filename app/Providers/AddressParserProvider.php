31f55eefef984093e638599b9bfcd564b1bce690
RevenkoVladislav <Vlad_reven@mail.ru>
2025-11-22 18:22:34 +0600

    подмена зависимости для AddressParser

--- app/Providers/AddressParserProvider.php
+++ app/Providers/AddressParserProvider.php
@@ -6,6 +6,12 @@ use App\Services\AddressParser\AddressParser;
 use App\Services\AddressParser\AddressParserInterface;
 use Illuminate\Support\ServiceProvider;
 
+class Mock implements AddressParserInterface{
+    public function parse(string $address) : array{
+        return [];
+    }
+}
+
 class AddressParserProvider extends ServiceProvider
 {
     /**
@@ -13,9 +19,13 @@ class AddressParserProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->singleton(AddressParserInterface::class, function () {
-            $conf = config('parsers')['address'];
-            return new AddressParser($conf['token'], $conf['secret']);
+//        $this->app->singleton(AddressParserInterface::class, function () {
+//            $conf = config('parsers')['address'];
+//            return new AddressParser($conf['token'], $conf['secret']);
+//        });
+
+        $this->app->singleton(AddressParserInterface::class, function() {
+           return new Mock();
         });
     }
 
